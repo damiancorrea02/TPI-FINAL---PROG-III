5a61d7a8b592b7c0d2c74b36de8446b5fca07ae9
unknown <eservetti2018@gmail.com>
2025-11-10 19:58:24 -0300

    Funciones de login

---
+++ js/login.js
@@ -0,0 +1,52 @@
+const emailInp = document.getElementById("email");
+const passInp = document.getElementById("password");
+const loginButton = document.getElementById("login-btn");
+
+const ENDPOINT = "https://69125f3052a60f10c82173d4.mockapi.io/tpi";
+
+loginButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try {
+    const userData = {};
+  const password = passInp.value;
+  const email = emailInp.value;
+  if (!password.trim() || !email.trim()) {
+    alert("Please enter all the data");
+  }
+
+  userData.email = email;
+  userData.password = password;
+  if (email === "servettiemilio1@gmail.com") {
+    userData.isAdmin = true;
+  } else userData.isAdmin = false;
+
+  // revisa si la cuenta ya existe
+  const usersResponse = await fetch(`${ENDPOINT}/Auth`);
+  const users = await usersResponse.json();
+
+  const exists = users.find((u) => u.email === email);
+
+  // si existe y la contraseña no es correcta, muestra un error
+  if (exists && exists.password !== password) {
+    return alert("Invalid data")
+  }
+
+  // si existe guarda la info en localstorage y redirecciona
+  if (exists) {
+    localStorage.setItem("user-data", JSON.stringify(userData));
+    location.replace("/index.html");
+  }
+  // sino, crea la cuenta y luego la guarda
+  else {
+    const response = await fetch(`${ENDPOINT}/Auth`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+    localStorage.setItem("user-data", JSON.stringify(userData));
+    location.replace("/index.html");
+  }
+  } catch (error) {
+   alert("Error signing in") 
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,77 +1,97 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
-
-
-    // Car Categories
-    $(".categories-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        dots: false,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fas fa-chevron-left"></i>',
-            '<i class="fas fa-chevron-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:3
-            }
-        }
-    });
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
     } else {
-        $('.back-to-top').fadeOut('slow');
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
     }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  });
 
+  // Car Categories
+  $(".categories-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 })(jQuery);
 
+// cambiar contenido del "login button" dependiendo si hay sesion iniciada o no
+const navBar = document.getElementById("navbarCollapse");
+const loginButton = document.getElementById("login-header");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const userData = localStorage.getItem("user-data");
+  const parsedUserData = JSON.parse(userData);
+  // si el user esta logeado, cambiar la funcionalidad del boton para que funcione como un logout
+  if (userData) {
+    loginButton.innerHTML = "Logout";
+    loginButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      localStorage.removeItem("user-data");
+      location.reload();
+    });
+  }
+
+  // si el user esta logeado y es admin, se habilita la funcion de manejar usuarios como admin
+  if (userData && parsedUserData.isAdmin) {
+    const adminPageButton = document.createElement("a");
+    adminPageButton.classList = "btn btn-primary rounded-pill py-2 px-4"
+    adminPageButton.innerHTML = "Admin";
+    adminPageButton.href = "/admin.html";
+    navBar.appendChild(adminPageButton);
+  }
+});
